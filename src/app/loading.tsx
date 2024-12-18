cd8e442579d9ab35c7789f56921d9781a3228777
as <ahmed.soliman@vconnct.com>
2024-12-18 17:50:45 +0200

    display posts

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import "./loading.css"
-import { Box, Typography } from '@mui/material'
-export default function Loading() {
+import * as React from "react";
+import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
+import { Container } from "@mui/material";
+
+export default function Variants() {
   return (
-    <Box sx={{display:"flex" , justifyContent:"center" , alignItems:"center"}}>
-    <Typography sx={{fontSize:"50px"}} className="loader">Loading</Typography>   
-    
-        </Box>
-  )
+    <Container maxWidth="sm">
+      <Stack spacing={1}>
+        {/* For variant="text", adjust the height via font-size */}
+        <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
+
+        {/* For other variants, adjust the size with `width` and `height` */}
+        <Skeleton variant="circular" width={40} height={40} />
+        <Skeleton variant="rectangular" width={210} height={60} />
+        <Skeleton variant="rounded" width={210} height={60} />
+      </Stack>
+    </Container>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,37 @@
-"use client"
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { storeDispatch, storeState } from '@/Redux/store';
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { storeDispatch, storeState } from "@/Redux/store";
 import { getPostts } from "@/Redux/slices/allPosts";
 import Loading from "./loading";
+import PostDetails from "./_Components/postdetails/PostDetails";
+import { Container } from "@mui/material";
 
-export default function Home(){
-  let {posts , isloading}= useSelector((state:storeState)=>state.postReducer)
-  let {push} = useRouter()
-  let dispatch = useDispatch<storeDispatch>()
-  useEffect(()=>{
-    if(!localStorage.getItem("token")){
-      push("/login")
+export default function Home() {
+  let { posts, isloading } = useSelector(
+    (state: storeState) => state.postReducer
+  );
+  let { push } = useRouter();
+  let dispatch = useDispatch<storeDispatch>();
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      push("/login");
+    } else {
+      dispatch(getPostts());
     }
-    else{
-      dispatch(getPostts())
-    }
-  },[])
+  }, []);
   return (
     <>
-    {isloading? <Loading/> :  <h1>Home page</h1>}
-   
+      {isloading ? (
+        <Loading />
+      ) : (
+        <Container maxWidth="sm">
+          {posts.map((post, index) => (
+            <PostDetails key="index" post={post} />
+          ))}
+        </Container>
+      )}
     </>
-  )
+  );
 }
